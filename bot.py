7bd9eda2cb867e5c10952c9af24572b534b5265d
kostyamorales <68538448+kostyamorales@users.noreply.github.com>
2020-10-27 15:41:36 +0300

    Add logging_bot, prepared for heroku

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv
-from os import getenv
+from os import environ
 import requests
 from time import sleep
 import telegram
+import logging
 
 
 def get_checklist(token, chat_id):
@@ -13,7 +13,7 @@ def get_checklist(token, chat_id):
         }
         url = f'https://dvmn.org/api/long_polling/'
         try:
-            response = requests.get(url, headers=headers, params=params, timeout=91)
+            response = requests.get(url, headers=headers, params=params, timeout=90)
             response.raise_for_status()
             answer = response.json()
             if answer['status'] == 'timeout':
@@ -29,16 +29,37 @@ def get_checklist(token, chat_id):
                     message = "К сожалению в работе нашлись ошибки"
                 bot.send_message(chat_id,
                                  f'Преподаватель проверил работу: "{name_work}" https://dvmn.org{lesson_url} {message}')
-        except requests.exceptions.ReadTimeout:
-            continue
-        except ConnectionError:
+        except requests.exceptions.ReadTimeout as error:
+            logger.info(error)
+        except ConnectionError as error:
+            logger.info(error)
             sleep(30)
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    telegram_api_key = getenv('TELEGRAM_API_KEY')
-    telegram_chat_id = getenv('TELEGRAM_CHAT_ID')
-    devman_token = getenv('DEVMAN_API_TOKEN')
-    bot = telegram.Bot(telegram_api_key)
-    get_checklist(devman_token, telegram_chat_id)
+    telegram_bot_token = environ['TELEGRAM_BOT_TOKEN']
+    telegram_chat_id = environ['TELEGRAM_CHAT_ID']
+    dvmn_token = environ['DVMN_API_TOKEN']
+    telegram_logging_bot_token = environ['TELEGRAM_LOGGING_BOT_TOKEN']
+    logging_bot = telegram.Bot(telegram_logging_bot_token)
+
+
+    class MyLogsHandler(logging.Handler):
+
+        def emit(self, record):
+            log_entry = self.format(record)
+            logging_bot.send_message(telegram_chat_id, log_entry)
+
+
+    logging.basicConfig(level='INFO', format='%(filename)s - %(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger()
+    my_handler = MyLogsHandler()
+    logger.addHandler(my_handler)
+    template_fmt = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s')
+    my_handler.setFormatter(template_fmt)
+    bot = telegram.Bot(telegram_bot_token)
+    logger.info('Бот запущен')
+    try:
+        get_checklist(dvmn_token, telegram_chat_id)
+    except Exception as error:
+        logger.info(f'Бот упал с ошибкой: {error}')
